6bb17a1b53b8e4c7924dccb0d06507bb576f7222
Luigi Quaranta <quarantalgu@gmail.com>
2020-10-23 11:06:48 +0200

    Basic main script

---
+++ KaggleTorrent/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    """This is the entrypoint for this CLI application.
+    Here we will handle possible arguments and orchestrate function/method calls
+    to build and populate the KaggleTorrent database"""
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
