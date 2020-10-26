aa464279f10b180ba4f87b9e1425aaeb8bc4c81a
Luigi Quaranta <quarantalgu@gmail.com>
2020-10-26 19:58:42 +0100

    Updates the POC based on the Kaggle API according to the changes made in the main scripts.

--- examples/download_notebooks_via_API.py
+++ examples/download_notebooks_via_API.py
@@ -6,42 +6,49 @@ This script queries the KaggleTorrent database and downloads the selected subset
     ``CREATE DATABASE IF NOT EXISTS kaggle_torrent CHARACTER SET utf8mb4;``
 """
 
-import os
-
 from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.rest import ApiException
-from sqlalchemy import create_engine, select, desc, and_
+from sqlalchemy import select, desc, and_
 
-from KaggleTorrent.buildDbSchema import (users, kernels, kernelLanguages, kernelVersions)
-
+# from KaggleTorrent.build_db_schema import (users, kernels, kernelLanguages, kernelVersions)
+from KaggleTorrent.build_db_schema import DbSchema
 # Configuration
 # TODO: Make this constant a command line argument.
+from KaggleTorrent.db_connection_handler import DbConnectionHandler
+
 DEST_FOLDER = './API_data/'
 
 # Kaggle API Authentication
 api = KaggleApi()
 api.authenticate()
 
-# Database connection
-mysql_username = os.environ['MYSQL_USER']
-mysql_password = os.environ['MYSQL_PWD']
-engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/kaggle_torrent'
-                       '?charset=utf8mb4'.format(mysql_username, mysql_password), pool_recycle=3600)
+# Create DB engine
+db_connection_handler = DbConnectionHandler()
+engine = db_connection_handler.create_sqlalchemy_engine()
+
+# Build the database schema
+db_schema = DbSchema(sqlalchemy_engine=engine)
 
 # Query the database
 with engine.connect() as connection:
     # Prepare the query
-    columns = [users.c.UserName + '/' + kernels.c.CurrentUrlSlug, kernels.c.TotalVotes, kernels.c.TotalViews]
+    columns = [db_schema.users.c.UserName + '/' + db_schema.kernels.c.CurrentUrlSlug,
+               db_schema.kernels.c.TotalVotes,
+               db_schema.kernels.c.TotalViews]
+
     s = select(columns)
-    s = s.select_from(users.join(kernels).join(kernelVersions).join(kernelLanguages))
+    s = s.select_from(
+        db_schema.users.join(db_schema.kernels)
+            .join(db_schema.kernel_versions)
+            .join(db_schema.kernel_languages))
     s = s.where(
         and_(
-            kernels.c.Medal == 3,
+            db_schema.kernels.c.Medal == 3,
             # users.c.PerformanceTier == 5,
-            kernelLanguages.c.Name == 'IPython Notebook'
+            db_schema.kernel_languages.c.Name == 'IPython Notebook'
         )
     )
-    s = s.order_by(desc(kernels.c.TotalVotes), desc(kernels.c.TotalViews))
+    s = s.order_by(desc(db_schema.kernels.c.TotalVotes), desc(db_schema.kernels.c.TotalViews))
 
     # TODO: this is just a POC. Remove this LIMIT statement once the query is verified.
     s = s.limit(20)
